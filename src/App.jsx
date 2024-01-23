ce764867cdbe50df24588e67fe98a8aeba2ab6d3
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-01-23 16:59:01 +0700

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className="bg-[rgba(255,255,255,1.0)] flex flex-row justify-center w-full">
-      <div className="bg-[rgba(255,255,255,1.0)] overflow-hidden border-r [border-right-style:solid] border-l [border-left-style:solid] border-transparent [background:linear-gradient(180deg,rgb(12.42,11.82,14.24)_0%,rgb(12.42,11.82,14.24)_0.01%,rgb(28.37,23.62,36.37)_27.82%,rgb(25.58,18.6,36.24)_55.91%,rgb(30.65,21.21,44.72)_73.91%,rgb(38,25,57)_100%)] w-full h-[5217px]">
+      <div className="bg-[rgba(255,255,255,1.0)] overflow-hidden border-r [border-right-style:solid] border-l [border-left-style:solid] border-transparent [background:linear-gradient(180deg,rgb(12.42,11.82,14.24)_0%,rgb(12.42,11.82,14.24)_0.01%,rgb(28.37,23.62,36.37)_27.82%,rgb(25.58,18.6,36.24)_55.91%,rgb(30.65,21.21,44.72)_73.91%,rgb(38,25,57)_100%)] w-full h-[4000px]">
         <div className="flex flex-col w-full items-center gap-[40px] relative -left-px">
           <div className="inline-flex flex-col items-center justify-center relative flex-[0_0_auto]">
             <div className="flex w-full items-center justify-between px-[40px] py-[25px] relative flex-[0_0_auto]">
@@ -112,112 +112,6 @@ export const App = () => {
               src="owallet-chatbot.svg"
             />
           </div>
-          <div className="flex flex-col items-center gap-[10px] px-[100px] py-[20px] relative self-stretch flex-[0_0_auto]">
-            <div className="flex w-[1267px] items-center justify-between relative flex-[0_0_auto] rounded-[25px]">
-              <div className="inline-flex items-start gap-[24px] relative flex-[0_0_auto]">
-                <img src="steps.svg" alt="" />
-                <div className="inline-flex flex-col items-start gap-[69px] relative flex-[0_0_auto]">
-                  <div className="inline-flex flex-col items-start gap-[4px] relative flex-[0_0_auto]">
-                    <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-[#ffffff] text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                      Select a Stock
-                    </div>
-                    <p className="relative w-[367px] font-text font-[number:var(--text-font-weight)] text-purple-text text-[length:var(--text-font-size)] tracking-[var(--text-letter-spacing)] leading-[var(--text-line-height)] [font-style:var(--text-font-style)]">
-                      Choose a stock you want to analyze or predict its future
-                      performance.
-                    </p>
-                  </div>
-                  <div className="flex-col w-[367px] items-start gap-[4px] flex-[0_0_auto] flex relative">
-                    <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-[#ffffff] text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                      Customize Parameters
-                    </div>
-                    <p className="relative self-stretch font-text font-[number:var(--text-font-weight)] text-purple-text text-[length:var(--text-font-size)] tracking-[var(--text-letter-spacing)] leading-[var(--text-line-height)] [font-style:var(--text-font-style)]">
-                      Tailor the prediction model by adjusting parameters to
-                      match your investment strategy.
-                    </p>
-                  </div>
-                  <div className="flex-col w-[367px] items-start gap-[4px] flex-[0_0_auto] flex relative">
-                    <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-[#ffffff] text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                      Get Accurate Predictions
-                    </div>
-                    <p className="relative self-stretch font-text font-[number:var(--text-font-weight)] text-purple-text text-[length:var(--text-font-size)] tracking-[var(--text-letter-spacing)] leading-[var(--text-line-height)] [font-style:var(--text-font-style)]">
-                      Wallie AI&#39;s advanced algorithm will generate
-                      predictions and provide real-time analysis to help you
-                      make informed decisions.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="inline-flex flex-col items-start gap-[30px] p-[60px] relative flex-[0_0_auto] rounded-[24px] [background:linear-gradient(180deg,rgb(34.41,27.38,49.66)_0%,rgba(44.24,36.84,60.28,0.24)_100%)]">
-                <div className="inline-flex flex-col items-start gap-[24px] relative flex-[0_0_auto]">
-                  <p className="relative w-fit mt-[-1.00px] [font-family:'SF_Mono-Light',Helvetica] font-light text-[#66646b] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
-                    What change of Orai price 10 days ago?
-                  </p>
-                  <div className="inline-flex items-start gap-[60px] relative flex-[0_0_auto]">
-                    <div className="inline-flex items-center gap-[16px] relative flex-[0_0_auto]">
-                      <div className="relative w-fit mt-[-1.00px] [font-family:'Lexend_Deca-Light',Helvetica] font-light text-purple-text text-[16px] tracking-[0] leading-[normal]">
-                        MODEL
-                      </div>
-                      <img
-                        className="relative w-[111px] h-[20px]"
-                        alt="Selection"
-                        src="selection.svg"
-                      />
-                    </div>
-                    <div className="inline-flex items-start gap-[16px] relative flex-[0_0_auto]">
-                      <div className="relative w-fit mt-[-1.00px] [font-family:'Lexend_Deca-Light',Helvetica] font-light text-purple-text text-[16px] tracking-[0] leading-[normal]">
-                        PARAMETER
-                      </div>
-                      <img
-                        className="relative w-[111px] h-[20px]"
-                        alt="Selection"
-                        src="selection.svg"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <img
-                  className="w-full h-px object-cover relative self-stretch"
-                  alt="Long Line"
-                  src="long-line.svg"
-                />
-                <div className="flex flex-col w-[510px] items-start relative flex-[0_0_auto]">
-                  <div className="relative w-[297px] h-[47px] mt-[-1.00px] [font-family:'SF_Mono-Light',Helvetica] font-light text-[#66646b] text-[16px] tracking-[0] leading-[normal]">
-                    Generating output...
-                  </div>
-                  <div className="flex flex-col items-start gap-[16px] relative self-stretch w-full flex-[0_0_auto]">
-                    <div className="flex items-start gap-[16px] relative self-stretch w-full flex-[0_0_auto]">
-                      <div className="w-[149px] bg-[#291a4580] relative h-[24px] rounded-[2px]" />
-                      <div className="flex-1 grow bg-[#41188f80] relative h-[24px] rounded-[2px]" />
-                    </div>
-                    <div className="flex items-start gap-[16px] relative self-stretch w-full flex-[0_0_auto]">
-                      <div className="w-[291px] bg-[#41188f80] relative h-[24px] rounded-[2px]" />
-                      <div className="flex-1 grow bg-[#291a4580] relative h-[24px] rounded-[2px]" />
-                      <div className="w-[47px] bg-[#291a4580] relative h-[24px] rounded-[2px]" />
-                    </div>
-                    <div className="w-[437px] bg-[#291a4580] relative h-[24px] rounded-[2px]" />
-                    <div className="inline-flex items-start gap-[16px] relative flex-[0_0_auto]">
-                      <div className="relative w-[100px] h-[24px] bg-[#41188f80] rounded-[2px]" />
-                      <div className="w-[100px] bg-[#291a4580] relative h-[24px] rounded-[2px]" />
-                    </div>
-                  </div>
-                </div>
-                <div className="inline-flex items-center justify-center gap-[4px] relative flex-[0_0_auto]">
-                  <div className="inline-flex items-start gap-[60px] relative flex-[0_0_auto]">
-                    <div className="inline-flex items-center gap-[16px] relative flex-[0_0_auto]">
-                      <div className="relative w-fit mt-[-1.00px] [font-family:'Lexend_Deca-Light',Helvetica] font-light text-purple-text text-[16px] tracking-[0] leading-[normal]">
-                        REGENERATE
-                      </div>
-                    </div>
-                  </div>
-                  <img
-                    className="relative w-[16px] h-[16px]"
-                    alt="Reload"
-                    src="reload.svg"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
           <div className="flex flex-col w-[1512px] items-center gap-[40px] px-0 py-[60px] relative flex-[0_0_auto]">
             <div className="inline-flex flex-col items-start gap-[45px] relative flex-[0_0_auto]">
               <div className="inline-flex flex-col items-start gap-[12px] relative flex-[0_0_auto]">
@@ -314,70 +208,6 @@ export const App = () => {
                 </p>
                 <div className="relative w-fit [font-family:'Lexend_Deca-Regular',Helvetica] font-normal text-purple-text text-[32px] tracking-[0] leading-[normal] whitespace-nowrap">
                   +
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex w-[1315px] items-start justify-between px-[100px] py-[62px] relative flex-[0_0_auto]">
-            <div className="inline-flex flex-col items-start gap-[24px] relative flex-[0_0_auto]">
-              <div className="inline-flex flex-col items-start gap-[45px] px-[16px] py-[8px] relative flex-[0_0_auto] bg-[#31195f] rounded-[21px]">
-                <div className="inline-flex flex-col items-start gap-[12px] relative flex-[0_0_auto]">
-                  <div className="relative w-fit mt-[-1.00px] [font-family:'SF_Mono-Regular',Helvetica] font-normal text-[#ffffff] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
-                    Pricing
-                  </div>
-                </div>
-              </div>
-              <div className="relative w-fit font-header-2 font-[number:var(--header-2-font-weight)] text-[#ffffff] text-[length:var(--header-2-font-size)] tracking-[var(--header-2-letter-spacing)] leading-[var(--header-2-line-height)] [font-style:var(--header-2-font-style)]">
-                Payment Gateway
-              </div>
-              <div className="flex flex-col w-[626px] items-start gap-[16px] relative flex-[0_0_auto]">
-                <div className="flex w-[626px] items-center gap-[816px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#231b31] rounded-[12px]">
-                  <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-purple-text text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                    Paypal
-                  </div>
-                </div>
-                <div className="flex w-[626px] items-center gap-[816px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#231b31] rounded-[12px]">
-                  <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-purple-text text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                    Amazon Pay
-                  </div>
-                </div>
-                <div className="flex w-[626px] items-center gap-[816px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#231b31] rounded-[12px]">
-                  <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-purple-text text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                    Stripe
-                  </div>
-                </div>
-                <div className="flex w-[626px] items-center gap-[816px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#231b31] rounded-[12px]">
-                  <div className="relative w-fit mt-[-1.00px] font-big-text font-[number:var(--big-text-font-weight)] text-purple-text text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                    Apple Pay
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="inline-flex flex-col items-start gap-[24px] relative flex-[0_0_auto]">
-              <div className="inline-flex flex-col items-start gap-[45px] px-[16px] py-[8px] relative flex-[0_0_auto] bg-[#31195f] rounded-[21px]">
-                <div className="inline-flex flex-col items-start gap-[12px] relative flex-[0_0_auto]">
-                  <div className="relative w-fit mt-[-1.00px] [font-family:'SF_Mono-Regular',Helvetica] font-normal text-[#ffffff] text-[16px] tracking-[0] leading-[normal] whitespace-nowrap">
-                    Subscribe
-                  </div>
-                </div>
-              </div>
-              <div className="relative w-fit font-header-2 font-[number:var(--header-2-font-weight)] text-[#ffffff] text-[length:var(--header-2-font-size)] tracking-[var(--header-2-letter-spacing)] leading-[var(--header-2-line-height)] [font-style:var(--header-2-font-style)]">
-                User Subcription
-              </div>
-              <p className="relative w-[545px] font-big-text font-[number:var(--big-text-font-weight)] text-purple-text text-[length:var(--big-text-font-size)] tracking-[var(--big-text-letter-spacing)] leading-[var(--big-text-line-height)] [font-style:var(--big-text-font-style)]">
-                Each user subscribes to weminal by sending <br /> your funds to
-                the on-chain bot
-              </p>
-              <div className="flex items-center gap-[10px] p-[24px] relative self-stretch flex-[0_0_auto] rounded-[12px] border border-solid border-[#6755a0]">
-                <div className="relative w-fit mt-[-1.00px] [font-family:'SF_Mono-Regular',Helvetica] text-[#66646b] text-[42px] font-normal tracking-[0] leading-[normal] whitespace-nowrap">
-                  0000
-                </div>
-              </div>
-              <div className="inline-flex flex-col items-center justify-center gap-[39px] relative flex-[0_0_auto]">
-                <div className="inline-flex items-center justify-center gap-[10px] px-[40px] py-[24px] relative flex-[0_0_auto] bg-[#21008233] rounded-[12px]">
-                  <div className="relative w-fit mt-[-1.00px] [font-family:'Lexend_Deca-Regular',Helvetica] font-normal text-[#ffffff] text-[24px] tracking-[0] leading-[normal]">
-                    Deposit
-                  </div>
                 </div>
               </div>
             </div>
